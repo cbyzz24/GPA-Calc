fa6259f4be18068affcdd0fd5f7bf4932ff3ccfd
cbyzz24 <155403160+cbyzz24@users.noreply.github.com>
2025-05-18 20:00:51 -0600

    If statement addition

--- main.js
+++ main.js
@@ -14,19 +14,24 @@ const listEl = document.getElementById("gradeList");
 let grades = [];
 
 function submitGrade(){
-    let listItem = document.createElement("p");
-    listItem.innerText = entryEl.value;
-    listEl.appendChild(listItem);
-    grades.push(listItem.innerHTML);
-    const totalValue = grades.reduce((currentTotal, grades) =>{
-        return parseFloat(grades) + currentTotal
-    }, 0);
-    const gradesLength = grades.length;
-    const gpa = totalValue/gradesLength;
-    resultEl.innerHTML = gpa.toFixed(1) + " %";
-    console.log(gpa);
-    entryEl.value = "";
-};
+    if(entryEl.value == ""){
+        alert("Please Enter a Grade Value");
+    } else{
+        let listItem = document.createElement("p");
+        listItem.innerText = entryEl.value;
+        listEl.appendChild(listItem);
+        grades.push(listItem.innerHTML);
+        const totalValue = grades.reduce((currentTotal, grades) =>{
+            return parseFloat(grades) + currentTotal
+        }, 0);
+        const gradesLength = grades.length;
+        const gpa = totalValue/gradesLength;
+        resultEl.innerHTML = gpa.toFixed(1) + " %";
+        // console.log(gpa);
+        entryEl.value = "";
+    }
+    
+}
 
 function clearResults(){
     grades = [];
